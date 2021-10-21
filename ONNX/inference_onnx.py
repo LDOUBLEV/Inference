7b5947cfa0c0b864e66fdf30b09256ce63a3d650
cuicheng01 <cuicheng_smile@163.com>
2021-10-21 03:16:56 +0000

    Add convert_paddle2onnx.sh & onnxruntime infer tool

---
+++ ONNX/inference_onnx.py
@@ -0,0 +1,118 @@
+import numpy as np
+import argparse
+import sys, os
+sys.path.insert(1, os.path.join(sys.path[0], ".."))
+import auto_log
+
+import onnx
+import onnxruntime as rt
+
+
+import time
+
+def str2bool(v):
+    return v.lower() in ("true", "t", "1")
+
+
+def init_args():
+    parser = argparse.ArgumentParser()
+    # params for prediction engine
+    parser.add_argument("--batch_size", type=int, default=1)
+    parser.add_argument("--num_thread", type=int, default=1)
+    parser.add_argument("--repeat_times", type=int, default=1000)
+    parser.add_argument("--warmup_times", type=int, default=100)
+    parser.add_argument("--use_gpu", type=bool, default=False)
+    parser.add_argument("--inputs_shape", type=str, default="3,224,224")
+    parser.add_argument("--onnx_file_path", type=str, default="./onnx_model_opset11/GhostNet_x0_5.onnx")
+    
+    return parser
+
+
+def parse_args():
+    parser = init_args()
+    return parser.parse_args()
+
+
+def build_sess(onnx_file_path="", use_gpu=False, num_thread=1):
+    so = rt.SessionOptions()
+    so.intra_op_num_threads = num_thread
+    so.execution_mode = rt.ExecutionMode.ORT_SEQUENTIAL
+    so.graph_optimization_level = rt.GraphOptimizationLevel.ORT_ENABLE_ALL
+    
+    
+    onnx_model = onnx.load_model(onnx_file_path)
+    sess = rt.InferenceSession(onnx_model.SerializeToString(), sess_options=so)
+    if use_gpu == True:
+        sess.set_providers(['CUDAExecutionProvider'])
+    else:
+        sess.set_providers(['CPUExecutionProvider'])
+    return sess
+
+    
+
+def inference(args):
+
+    bs = int(args.batch_size)
+    inputs_shape = [bs] + list(map(int, args.inputs_shape.split(",")))
+    assert len(inputs_shape)==4 and inputs_shape[1] == 3, ""
+
+    fake_inputs = np.ones(inputs_shape).astype(np.float32)
+    onnx_file_path = args.onnx_file_path
+
+    engine_file_path = onnx_file_path[:-4] + "onnx"
+
+    if os.path.exists(engine_file_path):
+        os.system(f"rm -rf {engine_file_path}")
+
+    onnx_name = os.path.basename(args.onnx_file_path)
+
+    pid = os.getpid()
+    autolog = auto_log.AutoLogger(
+                model_name=onnx_name,
+                model_precision="fp32",
+                batch_size=args.batch_size,
+                data_shape=inputs_shape,
+                save_path="./output/infer.log",
+                inference_config=None,
+                pids=pid,
+                process_name=None,
+                gpu_ids=0,
+                time_keys=[
+                    'preprocess_time', 'inference_time', 'postprocess_time'
+                ],
+                warmup=0)
+
+    # Do inference
+    print('Running inference on image fake_image ...'.format())
+
+    st = time.time()
+    sess = build_sess(onnx_file_path=onnx_file_path, use_gpu=args.use_gpu, num_thread=args.num_thread)
+    input_name = sess.get_inputs()[0].name
+    output_name = sess.get_outputs()[0].name
+    for i in range(args.warmup_times):
+        output = sess.run([output_name], {input_name : fake_inputs})
+        prob = np.squeeze(output[0])
+        #run()
+
+    print(f"run {args.warmup_times} times warmup done with {time.time() - st}s!")
+
+    st = time.time()
+    for i in range(args.repeat_times):
+        autolog.times.start()
+        # Preprocess
+        autolog.times.stamp()
+        # Run inference.
+        output = sess.run([output_name], {input_name : fake_inputs})
+        autolog.times.stamp()
+        # PostPreprocess.
+        prob = np.squeeze(output[0])
+        autolog.times.end(stamp=True)
+    print(f"run {args.repeat_times} times done with {time.time() - st}s!")
+    # Report log
+    autolog.report()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    inference(args)
+
