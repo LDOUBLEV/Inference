ee346fc5648e2df301ad95b9e6feada6ed831ce1
cuicheng01 <cuicheng_smile@163.com>
2021-10-21 10:40:51 +0000

    update onnx infer

--- ONNX/run_onnx_infer.sh
+++ ONNX/run_onnx_infer.sh
@@ -2,14 +2,13 @@
 
 FILENAME=$1
 dataline=$(cat ${FILENAME})
+USE_GPU=$2
 
-ONNX_MODEL_DIR="../../onnx_model_opset11"
+ONNX_MODEL_DIR="./onnx_model_opset11"
 IFS=$'\n'
 lines=(${dataline})
 batch_size=( 1 16 )
 NUM_THREADS=( 1 6 )
-USE_GPU=( True False )
-
 
 eval "mkdir output"
 status_log="./output/results.log"
@@ -29,20 +28,27 @@ function status_check(){
 for onnx_name in ${lines[*]}; do
     onnx_path="${ONNX_MODEL_DIR}/${onnx_name}"
     for bs in ${batch_size[*]}; do
-        for threads in ${NUM_THREADS[*]}; do
-            for use_gpu in ${USE_GPU[*]}; do
-                _save_log_path="./output/onnx_${onnx_name}_precision_fp32_batchsize_${bs}_threads_${threads}_usegpu_${use_gpu}.log"
-                cmd="python3.7 inference_onnx.py --onnx_file_path=${onnx_path}  --batch_size=${bs} --num_thread=${threads} --use_gpu=${use_gpu}> ${_save_log_path} 2>&1 "
+        if [ ${USE_GPU} = "False" ]; then
+            for threads in ${NUM_THREADS[*]}; do
+                _save_log_path="./output/onnx_${onnx_name}_precision_fp32_batchsize_${bs}_threads_${threads}_usegpu_False.log"
+                cmd="python3.7 inference_onnx.py --onnx_file_path=${onnx_path}  --batch_size=${bs} --num_thread=${threads} > ${_save_log_path} 2>&1 "
                 eval ${cmd}
-                echo ${cmd}
                 status_check $? "${cmd}" "${status_log}"
                 sleep 0.1
             done
-        done
+        elif [ ${USE_GPU} = "True" ]; then
+            _save_log_path="./output/onnx_${onnx_name}_precision_fp32_batchsize_${bs}_threads_${threads}_usegpu_True.log"
+            cmd="python3.7 inference_onnx.py --onnx_file_path=${onnx_path}  --batch_size=${bs} > ${_save_log_path} 2>&1 "
+            eval ${cmd}
+            status_check $? "${cmd}" "${status_log}"
+            sleep 0.1
+        else
+            echo "Does not support hardware other than CPU and GPU Currently!"
+        fi
     done
 done
 
-cmd="python3.7 ../utils/log_parser.py --log_path=./output/ --output_name=ONNX_onnx_fp32.excel.xlsx"
-eval $cmd
+#cmd="python3.7 ../utils/log_parser.py --log_path=./output/ --output_name=ONNX_onnx_fp32.excel.xlsx"
+#eval $cmd
 
 
