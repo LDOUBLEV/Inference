f6f738dd7cd69fca9a106a2f6c9aecd1e0087a3c
gaotingquan <gaotingquan@baidu.com>
2021-11-08 12:02:45 +0000

    unify command

--- OpenVINO/run_openvino_infer.sh
+++ OpenVINO/run_openvino_infer.sh
@@ -3,8 +3,9 @@ niter=1000
 nwarmup=100
 batchsize_list="1 16"
 nthreads_list="1 6"
-models_dir="./${tag}/VINO/"
-models=`ls $models_dir`
+filename=$1
+models_dir=$2
+models=`cat ${filename}`
 
 
 function status_check(){
@@ -19,27 +20,21 @@ function status_check(){
 }
 
 
+mkdir output
 for nthreads in ${nthreads_list}
 do
     for batchsize in ${batchsize_list}
     do
-        save_dir="./${tag}/benchmark_logs"
-        if [ ! -d $save_dir ]
-        then
-            mkdir -p $save_dir
-        fi
-
         for model in $models
         do
-            model_path=${models_dir}${model}/model.xml
+            model_path=${models_dir}/${model}/model.xml
             if [ -d $model_path ]
             then
                 echo "The model no exists:"${model}
-                echo $model_path
                 continue
             fi
             sleep 10s
-            python3 -u ./OpenVINO/inference_openvino.py -m ${model_path} --niter ${niter} --nwarmup ${nwarmup} --nthreads ${nthreads} --batch_size ${batchsize} --save_path ./autolog/${model}_bs_${batchsize}_threads_${nthreads}.log &> ${save_dir}/${model}_bs_${batchsize}_threads_${nthreads}.log
+            python3 -u ./inference_openvino.py -m ${model_path} --niter ${niter} --nwarmup ${nwarmup} --nthreads ${nthreads} --batch_size ${batchsize} --save_path ./autolog/${model}_bs_${batchsize}_threads_${nthreads}.log &> ./output/${model}_bs_${batchsize}_threads_${nthreads}.log
             status_check $? "${cmd}" "${status_log}"
             echo "Done: $model."
         done
